ea7d67e657492cb908f6b75f9ed855319668b6f1
Muhammed Adhil <millionaireatage18@gmail.com>
2026-08-07 21:30:21 +0530

    Completed order system and My Orders page

--- app/my-orders/page.tsx
+++ app/my-orders/page.tsx
@@ -19,7 +19,15 @@ export default function MyOrdersPage() {
 
     const { data, error } = await supabase
       .from("orders")
-      .select("*")
+      .select(`
+  *,
+  order_items (
+    product_name,
+    price,
+    quantity,
+    image
+  )
+`)
       .eq("user_id", user.id)
       .order("created_at", { ascending: false });
 
@@ -50,6 +58,33 @@ export default function MyOrdersPage() {
             <p>📍 {order.address}</p>
             <p>💰 ₹{order.total_price}</p>
             <p>📦 {order.status}</p>
+            <hr className="my-3" />
+
+<h3 className="font-semibold mb-2">Products</h3>
+
+{order.order_items?.map((item: any, index: number) => (
+  <div
+    key={index}
+    className="flex justify-between items-center mb-2"
+  >
+    <div className="flex items-center gap-3">
+      <img
+        src={item.image}
+        alt={item.product_name}
+        className="w-12 h-12 rounded"
+      />
+
+      <div>
+        <p>{item.product_name}</p>
+        <p className="text-sm text-gray-500">
+          Qty: {item.quantity}
+        </p>
+      </div>
+    </div>
+
+    <p>₹{item.price}</p>
+  </div>
+))}
           </div>
         ))
       )}
